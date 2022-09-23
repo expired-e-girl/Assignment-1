c8823c027b6b41b1ccd623a4591728cf4819720c
Expired e Girl <partwolfpartpizza@gmail.com>
2022-09-22 23:59:55 -0400

    Oopsie

--- src/CoinMachine.java
+++ src/CoinMachine.java
@@ -10,31 +10,39 @@
 public class CoinMachine {
     public static void main(String[] args) {
 
-        int cash = Integer.parseInt(args[0]); //Amount dropped in by customer
+        //Amount dropped in by customer
+        int cash = Integer.parseInt(args[0]);
         System.out.println("Amount received: " + cash);
 
-        int price = Integer.parseInt(args[1]); //Cost of the item bought by customer
+        //Cost of the item bought by customer
+        int price = Integer.parseInt(args[1]);
         System.out.println("Price of the item: " + price);
 
-        int change = cash - price;  //Calculate owed change
+        //Calculate owed change
+        int change = cash - price;
         System.out.println("Required change: " + change);
 
-        int toonies = change/200; //
+        //Calculate amount of toonies that could be given back in change
+        int toonies = change/200;
         change %= 200;
         System.out.println("Toonies x " + toonies);
 
+        //Calculate amount of loonies that could be given back in change
         int loonies = change/100;
         change %= 100;
         System.out.println("Loonies x " + loonies);
 
+        //Calculate amount of quarters that could be given back in change
         int quarters = change/25;
         change %= 25;
         System.out.println("Quarters x " + quarters);
 
+        //Calculate amount of dimes that could be given back in change
         int dimes = change/10;
         change %= 10;
         System.out.println("Dimes x " + dimes);
 
+        //Calculate amount of nickels that could be given back in change
         int nickels = change/5;
         change %= 5;
         System.out.println("Nickels x " + nickels);
